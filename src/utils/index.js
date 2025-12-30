10a7749e3fe5f81e973c0587a5e94b9a35760f1c
spock <100180995+spockverse@users.noreply.github.com>
2025-12-30 14:05:40 -0800

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,3 @@
-
-
-
-export function createPageUrl(pageName: string) {
-    return '/' + pageName.toLowerCase().replace(/ /g, '-');
+export function createPageUrl(pageName) {
+  return '/' + pageName.toLowerCase().replace(/ /g, '-');
 }
